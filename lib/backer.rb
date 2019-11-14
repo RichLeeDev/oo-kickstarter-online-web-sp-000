e6980aa78829aacc332a44d99aa1a3f130c541da
Richard Lee <richlee81192@gmail.com>
2019-11-14 22:45:06 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,12 +1,12 @@
 class Backer
-  attr_accessor :name, :backed_projects
+  attr_accessor :name, :projects
   def initialize(name)
     @name = name
-    @backed_projects = []
+    @projects = []
   end
   
   def back_project(project)
-    @backed_projects << project
+    @projects << project
     project.backers << self
   end
 
